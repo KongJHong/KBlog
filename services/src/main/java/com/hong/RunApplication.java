778dd09ec4ee2c590d737214728db02a7b102347
jiangjianfeng <nail_candy@163.com>
2020-06-25 01:49:15 +0800

    首次上传

---
+++ services/src/main/java/com/hong/RunApplication.java
@@ -0,0 +1,17 @@
+package com.hong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author : KongJHong
+ * @Date : 2020-06-25 1:42
+ * @Version : 1.0
+ * Description     : 启动类
+ */
+@SpringBootApplication
+public class RunApplication {
+     public static void main(String[] args) {
+        SpringApplication.run(RunApplication.class,args);
+    }
+}
